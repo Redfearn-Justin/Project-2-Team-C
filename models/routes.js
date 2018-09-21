4764e08a8d9e12a8411bca409dd1cd38e668d19f
Napoleon <wanderofrazgriz@gmail.com>
2018-09-20 21:31:58 -0700

    minor tweaks to various files

--- models/routes.js
+++ models/routes.js
@@ -7,20 +7,26 @@ var routes = {
 
     //Select method
     select: function(cb) {
+
       orm.selectAll("game_Log", function(res) {
+
         cb(res);
       });
     },
 
     //Insert method; The variables cols and vals are arrays.
     insert: function(cols, vals, cb) {
+
       orm.insertOne("game_Log", cols, vals, function(res) {
+
         cb(res);
       });
     },
     //Update method
     update: function(objColVals, condition, cb) {
+
       orm.updateOne("game_Log", objColVals, condition, function(res) {
+        
         cb(res);
       });
     }
