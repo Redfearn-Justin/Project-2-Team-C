4e8d343500915b37caae80c1c61f660f0e173364
Napoleon <wanderofrazgriz@gmail.com>
2018-09-23 17:06:03 -0700

    first function fleshed out

--- db/justinSQL_function.js
+++ db/justinSQL_function.js
@@ -4,35 +4,61 @@
 
 //function for buttons pressed on the Leaderboard page
 
-function leaderboardDisplay() {
+//'buttonPressed' should be the "data-attribute", or something similar, to that of the corresponding "scoring metric" in question
+
+function leaderboardDisplay(buttonPressed) {
 
     connection.connect(function(err) {
 
         if(err) throw err;
-    
-        //buttonPressed should be dynamic to the button's "data attribute"
-        //change below to reflect dynamic button input
-    
-        var buttonPressed = "scrap amount";
-    
-        var sqlQuery = "SELECT captain_name, ship_name, scrap_amount, crew_hp, proficiency_points FROM game_Log ORDER BY " + buttonPressed + " DESC LIMIT 5"
-    
-        connection.query(sqlQuery, function(err, results) {
-    
-            if(err) throw err;
 
-            // pushing results to page
-            //make placeholder for display div
+
+        var sqlQuery = "SELECT captain_name, ship_name, scrap_amount, crew_hp, proficiency_points FROM game_Log ORDER BY " + buttonPressed + " DESC LIMIT 5";
+    
+
+        connection.query(sqlQuery, function(err, results) {
+
+            //constructing the table view of the leaderboard
+
+            var htmlTable = "<table id='leaderboard'>"
+
+            //table headers
+
+            htmlTable += "<tr> <th> Rank </th> <th> Captain Name </th> <th> " + buttonPressed + " </th> </tr>";
+
+            //variable to establish rank
+    
+            var placementNumber = 0;
+
+            //For loop for going through
 
             for (var i = 0; i < results.length; i++) {
-                $("#displayDiv").text() 
 
+                placementNumber++;
+
+                htmlTable += "<tr>"
+
+                //entering rank into 1st column
+
+                htmlTable += "<td>" + placementNumber + "</td>";
+
+                //entering captain's name into 2nd column 
+
+                htmlTable += "<td>" + results[i].captain_name + "</td>";
+
+                //entering score amount into final column (might need to fix, as "buttonPressed" is a 'dynamic variable')
+
+                htmlTable += "<td>" + results[i].buttonPressed + "</td>";
+
+                //close the table row after making row
+                htmlTable += "</tr>"
             }
 
+            // We close the table
+            htmlTable += "</table>";
 
-    
-    
-    
+            // Finally we send the user the HTML file we dynamically created.
+            res.send(htmlTable);
     
         });
     });
@@ -50,6 +76,9 @@ function scenerioButton () {
 
     // could make this a variable per the button being clicked
 //
+
+
+//fix export? (appears as "void");
 
 module.exports = {
     leaderboardDisplay: leaderboardDisplay,
