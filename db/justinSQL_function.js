b8e45b3ffe6e5538f1347fb3831aed11c91f5268
Napoleon <wanderofrazgriz@gmail.com>
2018-09-24 19:15:58 -0700

    updated Leaderboard function

--- db/justinSQL_function.js
+++ db/justinSQL_function.js
@@ -6,14 +6,16 @@
 
 //'buttonPressed' should be the "data-attribute", or something similar, to that of the corresponding "scoring metric" in question
 
-function leaderboardDisplay(buttonPressed) {
+//'buttonPressedSQL' is for the parallel variable, as to 'buttonPressed', but is in the EXACT form as in database
+
+function leaderboardDisplay(buttonPressedSQL, buttonPressed ) {
 
     connection.connect(function(err) {
 
         if(err) throw err;
 
 
-        var sqlQuery = "SELECT captain_name, ship_name, scrap_amount, crew_hp, proficiency_points FROM game_Log ORDER BY " + buttonPressed + " DESC LIMIT 5";
+        var sqlQuery = "SELECT captain_name, ship_name, scrap_amount, crew_hp, proficiency_points FROM game_Log ORDER BY " + buttonPressedSQL + " DESC LIMIT 5";
     
 
         connection.query(sqlQuery, function(err, results) {
@@ -48,16 +50,17 @@ function leaderboardDisplay(buttonPressed) {
 
                 //entering score amount into final column (might need to fix, as "buttonPressed" is a 'dynamic variable')
 
-                htmlTable += "<td>" + results[i].buttonPressed + "</td>";
+                htmlTable += "<td>" + results[i].buttonPressedSQL + "</td>";
 
                 //close the table row after making row
                 htmlTable += "</tr>"
             }
 
-            // We close the table
+            // closing table
             htmlTable += "</table>";
 
             // Finally we send the user the HTML file we dynamically created.
+            //if doesn't work properly, consider 'traditionally' inserting table into HTML
             res.send(htmlTable);
     
         });
